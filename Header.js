f729f8690d987041d19e1070a7848272e26c4685
Ashish-Kaintura <110974134+Ashish-Kaintura@users.noreply.github.com>
2022-09-06 22:35:18 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const Header = () => {
+   return (
+       <header>
+           <h1>ToDo</h1>
+           <span>GYM Routine</span>
+       </header>
+   );
+};
+ 
+export default Header;
